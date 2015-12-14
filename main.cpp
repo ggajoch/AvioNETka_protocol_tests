34b56883c31733dd01a87d0c9624c9aa7bdb135c
Grzegorz Gajoch <gajoch@gmail.com>
2015-12-14 02:26:22 +0100

    another test mock

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <protocol/NetworkLayer.h>
 #include <protocol/ApplicationLayer.h>
 
-
 extern "C" {
 #include <FreeRTOS.h>
 #include <queue.h>
@@ -23,20 +22,21 @@ void secondCall(float x) {
 }
 FloatDataDescriptor data2({2, true}, secondCall);
 
-FloatDataDescriptor data3({2, true});
+FloatDataDescriptor data3({2, false});
 
 
 xQueueHandle RxQueue, TxQueue;
 class FreeRTOSMock : public PHYInterface {
     void sendPacket(PHYDataStruct & data) {
-        printf("[RTOS] sending packet: ");
+        printf("[RTOS] data receiving from module: ");
         print_byte_table(data.data, data.len);
 
         xQueueSend(TxQueue, &data, portMAX_DELAY);
     }
 public:
     void mockData(PHYDataStruct * data) {
-        printf("MOCKING DATA\n");
+        printf("[RTOS] sending data to module: ");
+        print_byte_table(data->data, data->len);
         net->dataReceived(*data);
     }
 };
@@ -45,37 +45,27 @@ public:
 ApplicationLayer * app;
 FreeRTOSMock PHYFree;
 
-void taskTx(void * p) {
+
+void TaskMockPC(void * p) {
     while(1) {
         PHYDataStruct data;
-        xQueueReceive(TxQueue, &data, portMAX_DELAY);
-        printf("[RTOS] checking packet: %d\n", app->ackRequired(data.data[0]));
-
-        if( app->ackRequired(data.data[0]) ) {
-            printf("[RTOS] sending ACK\n");
-            //send ACK
-            NetworkDataStruct ack = makeACKPacket();
-            PHYDataStruct ph;
-            ph.append(ack.id);
-            ph.append(ack.data, ack.len);
-            xQueueSend(RxQueue, &ph, portMAX_DELAY);
+        if (xQueueReceive(TxQueue, &data, portMAX_DELAY)) {
+            //data received from module
+            printf("[MOCK]: data received!: ");
+            print_byte_table(data.data, data.len);
+            if (app->ackRequired(data.data[0])) {
+                //send ACK
+                printf("[MOCK] Sending ACK\n");
+                //vTaskDelay(100);
+                printf("[MOCK] Wait end\n");
+                NetworkDataStruct ack = makeACKPacket();
+                PHYDataStruct ph;
+                ph.append(ack.id);
+                ph.append(ack.data, ack.len);
+                PHYFree.mockData(&ph);
+            }
         }
-        if( data.data[0] == 1 ) {
-            vTaskDelay(10000);
-            printf("\n\n\nLOOP BACK DATA!!!\n\n\n");
-            xQueueSend(RxQueue, &data, portMAX_DELAY);
-        }
-    }
-}
-
-void taskRx(void * p) {
-    int c = 5;
-    while(1) {
-        PHYDataStruct data;
-        xQueueReceive(RxQueue, &data, portMAX_DELAY);
-        printf("[RTOS] receiving packet!!!\n");
-        vTaskDelay(1000);
-        PHYFree.mockData(&data);
+        //vTaskDelay(1);
     }
 }
 
@@ -83,44 +73,50 @@ void starter(void * p) {
     DataDescriptor * descriptors[] = {&data, &data2, &data3};
     ApplicationLayer appX(&PHYFree, descriptors, 3);
     app = &appX;
-    app->_sendSubscriptions();
+
+    printf("Hello!!!\n");
+    PHYDataStruct data;
+    data.data[0] = 252;
+    data.len = 1;
+    PHYFree.mockData(&data);
+
+    xSemaphoreHandle x = xSemaphoreCreateBinary();
+    xSemaphoreTake(x, 0);
     while(1) {
-        vTaskDelay(portMAX_DELAY);
+        xSemaphoreTake(x, portMAX_DELAY);
     }
 }
 
 void test(void * p) {
+    portENTER_CRITICAL();
+    xSemaphoreTake(app->link_established, portMAX_DELAY);
+    xSemaphoreGive(app->link_established);
+    portEXIT_CRITICAL();
+
     app->sendData(data, false);
 
     app->sendData(data, true);
 
     app->sendData(data2, 1.5f);
     while(1) {
-        printf("\n\n\n---------------------------------\n\n\n");
         app->sendData(data2, 1.5f);
-        vTaskDelay(30000);
+        printf("---------------------------\n\n");
+        vTaskDelay(2000);
     }
     vTaskDelay(portMAX_DELAY);
 }
+
+
 
 int main() {
     RxQueue = xQueueCreate(100, sizeof(DataDescriptor));
     TxQueue = xQueueCreate(100, sizeof(DataDescriptor));
 
-    xTaskCreate(taskRx, "Rx", 1000, NULL, 5, NULL);
-    xTaskCreate(taskTx, "Rx", 1000, NULL, 2, NULL);
+    xTaskCreate(TaskMockPC, "Rx", 1000, NULL, 3, NULL);
     xTaskCreate(starter, "st", 1000, NULL, 4, NULL);
     xTaskCreate(test, "ts", 1000, NULL, 1, NULL);
 
     vTaskStartScheduler();
-
-
-
-    /*app.sendData(data, false);
-
-    app.sendData(data, true);
-
-    app.sendData(data2, 1.5f);*/
 
 
     return 0;
